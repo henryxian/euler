ced1aa17d1525354f7f11458b29c105015d9442f
henryxian <henryxian@qq.com>
2013-06-09 11:38:06 +0800

    euler problem 2

---
+++ Even Fibonacci numbers.py
@@ -0,0 +1,20 @@
+def even(num):
+	if num < 2:
+		return "no even-valued term found"
+	elif num >= 2 and num < 8:
+		return 2
+	elif num == 8:
+		return 10
+	else:
+		even1 = 2
+		even2 = 8
+		total = 10
+		while even2 <= num:
+			#every third term in Fibonacci is even.
+			even1, even2 = even2, even2 * 4 + even1
+			if even2 <= num:
+			#add up the terms not exceed the limit num.
+				total += even2
+		return total
+if __name__ == '__main__':
+	print even(34)
