8d6df43ef97d31b018c54d42c6768053f2fc93f7
henryxian <henryxian@qq.com>
2013-06-09 11:39:25 +0800

    euler problem 3

--- max_prime_factor.py
+++ max_prime_factor.py
@@ -3,6 +3,6 @@ def max_prime_factor(num):
 	for i in range(2, int(sqrt(num)) + 1):
 		if num % i == 0:
 			return max_prime_factor(num / i)
-	print num
+	return num
 if __name__ == '__main__':
-	max_prime_factor(600851475143)
+	print max_prime_factor(600851475143)
